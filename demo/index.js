39740647aaf0981e6fd422aab84c04a6576472e4
Kentaro Ozeki <kentaro.ozeki@gmail.com>
2019-07-05 16:05:44 +0900

    Fix typing

--- demo/index.js
+++ demo/index.js
@@ -4,14 +4,14 @@ const client = feathers()
 client.configure(feathers.socketio(socket))
 client.configure(feathers.authentication({　storage: window.localStorage　}))
 
-UsersAuth = window.httpVueLoader('/components/UsersAuth.vue')
-UsersProfile = window.httpVueLoader('/components/UsersProfile.vue')
-Person = window.httpVueLoader('/components/Person.vue')
-Item = window.httpVueLoader('/components/Item.vue')
-FlowList = window.httpVueLoader('/components/FlowList.vue')
-Flow = window.httpVueLoader('/components/Flow.vue')
-FlowEdit = window.httpVueLoader('/components/FlowEdit.vue')
-Dashboard = window.httpVueLoader('/components/Dashboard.vue')
+const UsersAuth = window.httpVueLoader('/components/UsersAuth.vue')
+const UsersProfile = window.httpVueLoader('/components/UsersProfile.vue')
+const Person = window.httpVueLoader('/components/Person.vue')
+const Item = window.httpVueLoader('/components/Item.vue')
+const FlowList = window.httpVueLoader('/components/FlowList.vue')
+const Flow = window.httpVueLoader('/components/Flow.vue')
+const FlowEdit = window.httpVueLoader('/components/FlowEdit.vue')
+const Dashboard = window.httpVueLoader('/components/Dashboard.vue')
 
 const routes = [
 	{ path: '/users-auth', component: UsersAuth },
