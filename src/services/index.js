0a46645767dc08e6a0d5647916568db12f3c3333
Kentaro Ozeki <kentaro.ozeki@gmail.com>
2019-07-19 00:55:54 +0900

    Update exports for new services

--- src/services/index.js
+++ src/services/index.js
@@ -1,9 +1,13 @@
 const hr = require('./hr/hr.service.js');
 const item = require('./item/item.service.js');
 const users = require('./users/users.service.js');
+const plan = require('./plan/plan.service.js');
+const feed = require('./feed/feed.service.js');
 // eslint-disable-next-line no-unused-vars
 module.exports = function (app) {
   app.configure(hr);
   app.configure(item);
   app.configure(users);
+  app.configure(plan);
+  app.configure(feed);
 };
