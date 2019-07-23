b7687ba0ca13dcba75ee1cd0ebd712966088578d
Kentaro Ozeki <kentaro.ozeki@gmail.com>
2019-07-23 16:57:01 +0900

    Update scripts

--- script/populatePlan.js
+++ script/populatePlan.js
@@ -3,9 +3,20 @@ const request = require('request-promise');
 
 const BASE_URL = 'http://localhost:3030';
 
-console.log(plan1);
-request
-  .post({ url: `${BASE_URL}/api/plan/`, json: true, body: plan1 })
-  .then(res => {
-    console.log(res);
+(async function() {
+  let resp = await request.post({
+    url: `${BASE_URL}/api/authentication`,
+    json: true,
+    body: { strategy: 'local', email: 'demo1@example.com', password: 'demo1' }
   });
+  let token = resp.accessToken;
+  // console.log(token);
+  console.log(plan1);
+  resp = await request.post({
+    url: `${BASE_URL}/api/plan/`,
+    headers: { 'Authorization': token },
+    json: true,
+    body: plan1,
+  });
+  console.log(resp);
+})();
