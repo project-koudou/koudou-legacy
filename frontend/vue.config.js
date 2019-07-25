6e48892e83fbf4067a062e0ccbbb6814bda8bb27
Kentaro Ozeki <kentaro.ozeki@gmail.com>
2019-07-26 01:02:11 +0900

    Fix vue-cli config

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,6 +1,18 @@
 module.exports = {
   lintOnSave: false,
   devServer: {
-    proxy: 'http://localhost:3030',
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3030',
+        // pathRewrite: { '^/api': '/' },
+        changeOrigin: true,
+      },
+      '/socket.io': {
+        target: 'http://localhost:3030/',
+        // pathRewrite: { '^/socket.io': '/' },
+        changeOrigin: true,
+        ws: true,
+      },
+    },
   },
 };
