9ab355c35017309b8f867e189816ce3ea7e8546a
Kentaro Ozeki <kentaro.ozeki@gmail.com>
2019-07-27 16:06:38 +0900

    Update data, fixtures, and scripts

--- script/populatePlan.js
+++ script/populatePlan.js
@@ -6,3 +6,11 @@ const plan1 = require('../fixture/plan1.json');
   console.log(user1);
   app.service('api/plan').create(plan1, { user1 });
 })();
+
+const plan2 = require('../fixture/plan2.json');
+
+(async () => {
+  let user2 = await app.service('api/users').get('demo2@example.com');
+  console.log(user2);
+  app.service('api/plan').create(plan2, { user2 });
+})();
