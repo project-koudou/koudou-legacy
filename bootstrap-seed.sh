0a91ca832c9f0206543df4c2aff2f3886dd914e2
Kentaro Ozeki <kentaro.ozeki@gmail.com>
2019-07-29 02:58:41 +0900

    Add demo switch

--- bootstrap-seed.sh
+++ bootstrap-seed.sh
@@ -2,8 +2,10 @@
 
 set -eu
 
-node script/populateUsers.js  # Populate Users
-node script/populatePlan.js  # Populate Plan
-node script/populateFeed.js  # Populate Plan
-node script/populateItem.js  # Populate Item
-# curl -X POST http://localhost:9000/red/flows -H "Content-Type: application/json" --data "@../nodered-lib/flows/flows-all.json"  # Node-RED
+if [ -n "${DEMO-}" ]; then
+  node script/populateUsers.js  # Populate Users
+  node script/populatePlan.js  # Populate Plan
+  node script/populateFeed.js  # Populate Plan
+  node script/populateItem.js  # Populate Item
+  # curl -X POST http://localhost:9000/red/flows -H "Content-Type: application/json" --data "@../nodered-lib/flows/flows-all.json"  # Node-RED
+fi
